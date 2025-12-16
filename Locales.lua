aebc8c67a4fd7b2f4c46bffb1398bccbb1af30c6
StormFX <stormfx@gmail.com>
2025-12-16 09:48:44 -0600

    - Update `zhTW`. (BNS333)(Closes #69)

--- Locales.lua
+++ Locales.lua
@@ -50,5 +50,9 @@ elseif Locale == "ruRU" then
 	L["An alternate version of Serenity with modified Checked and Equipped textures."] = "Альтернативная версия Serenity с измененными текстурами Checked и Equipped."
 	L["An alternate version of Serenity Square with modified Checked and Equipped textures."] = "Альтернативная версия Serenity Square с измененными текстурами Checked и Equipped."
 --elseif Locale == "zhCN" then
---elseif Locale == "zhTW" then
+elseif Locale == "zhTW" then
+	L["A port of the original Serenity skin by Sairen."] = "這是 Sairen 原版 Serenity 外觀的移植版。"
+	L["A port of the original Serenity Square skin by Sairen."] = "這是 Sairen 原版 Serenity Square 外觀的移植版。"
+	L["An alternate version of Serenity Square with modified Checked and Equipped textures."] = "Serenity Square 的另一個版本，但有修改過的確認與裝備材質。"
+	L["An alternate version of Serenity with modified Checked and Equipped textures."] = "Serenity 的另一個版本，但有修改過的確認與裝備材質。"
 end
